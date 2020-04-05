342d06e0357c2d906595dcdc46fdfbb8e7c4a5dc
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-04-05 21:19:16 +0300

    add sol

--- server.js
+++ server.js
@@ -1,50 +1,53 @@
-'use strict';
+"use strict";
 
-const express     = require('express');
-const session     = require('express-session');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const auth        = require('./app/auth.js');
-const routes      = require('./app/routes.js');
-const mongo       = require('mongodb').MongoClient;
-const passport    = require('passport');
-const cookieParser= require('cookie-parser')
-const app         = express();
-const http        = require('http').Server(app);
-const sessionStore= new session.MemoryStore();
-
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const auth = require("./app/auth.js");
+const routes = require("./app/routes.js");
+const mongo = require("mongodb").MongoClient;
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const app = express();
+const http = require("http").Server(app);
+const sessionStore = new session.MemoryStore();
+const io = require("socket.io")(http);
 
 fccTesting(app); //For FCC testing purposes
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  key: 'express.sid',
-  store: sessionStore,
-}));
+process.env.SESSION_SECRET = "random";
+process.env.DATABASE =
+  "mongodb+srv://dbMahmoud:asdf3456@cluster0-cifau.mongodb.net/test?retryWrites=true&w=majority";
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    key: "express.sid",
+    store: sessionStore,
+  })
+);
 
-mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) console.log('Database error: ' + err);
-  
+mongo
+  .connect(process.env.DATABASE)
+  .then((client) => {
+    var db = client.db("dbMahmoud");
+
     auth(app, db);
     routes(app, db);
-      
+
     http.listen(process.env.PORT || 3000);
 
-  
-    //start socket.io code  
-
-  
+    //start socket.io code
 
     //end socket.io code
-  
-  
-});
+  })
+  .catch((err) => console.log(err));
